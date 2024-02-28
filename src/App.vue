91b4249faf8c51deee9ee7a261b143dfe199e730
PISCES <45836182+eatfishs@users.noreply.github.com>
2024-02-28 16:35:36 +0800

    去除嵌套简化demo

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,11 @@
 <template>
   <div style="width: 1000px">
-    <Table>
-      <template #name>name </template>
-      <template #age="{ record }">
-        <Cell icon="eye-filled" />
-        <Cell
-          icon="download"
-          :class="
-            !record.originalInfo ? 'cursor-disabled' : ''
-          "
-        />
-      </template>
-      <template #address>
-        <Cell icon="delete-filled" />
-        <Cell icon="eye-filled" />
-      </template>
-    </Table>
+    <Table> </Table>
   </div>
 </template>
 
 <script setup lang="ts">
 import Table from "./components/Table.vue";
-import Cell from "./components/Cell.vue";
 </script>
 
 <style scoped lang="less"></style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -9,7 +9,18 @@
   </div>
   <Table :dataSource="dataSource" :columns="columns">
     <template #bodyCell="data">
-      <slot :name="data.column.key" v-bind="data || {}"></slot>
+      <template v-if="data.column.dataIndex === 'name'">name </template>
+      <template v-if="data.column.dataIndex === 'age'"
+        ><Cell icon="eye-filled" />
+        <Cell
+          icon="download"
+          :class="!data.record.originalInfo ? 'cursor-disabled' : ''"
+        />
+      </template>
+      <template v-if="data.column.dataIndex === 'address'"
+        ><Cell icon="eye-filled" />
+        <Cell icon="delete-filled" />
+      </template>
     </template>
   </Table>
 </template>
@@ -17,6 +28,9 @@
 <script setup lang="ts">
 import { ref, computed } from "vue";
 import { Table, Button } from "ant-design-vue";
+import Cell from "./Cell.vue";
+// 将这里注释掉后就不会出现问题
+// :class="!data.record.originalInfo ? 'cursor-disabled' : ''"
 const basicColumns = ref([
   {
     title: "姓名",
